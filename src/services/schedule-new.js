d1a2e204306cc80297f0778625d54c8d710ea4a1
Israel Teixeira <israeltx@gmail.com>
2025-06-20 15:59:59 -0300

    Create scheduling service

---
+++ src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import { schedulesDay } from "../modules/schedules/load";
+import { apiConfig } from "./api-config.js";
+
+export async function schedulesNew({id, name, when}) {
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id, name, when})
+    })
+    alert('Agendamento realizado com sucesso')
+  } catch (error) {
+    alert('Não foi possível agendar')
+    console.log(error);
+  }
+}
