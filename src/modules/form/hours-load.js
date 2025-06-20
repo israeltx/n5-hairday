a27207ce6a995c22ca179b6adceeebf8b9ccf5dd
Israel Teixeira <israeltx@gmail.com>
2025-06-20 14:14:31 -0300

    Render available hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,9 @@
 import dayjs from 'dayjs';
 import { openingHours } from '../../utils/opening-hours.js'
 
+// Select the hours list
+const hours = document.getElementById('hours')
+
 export function hoursLoad({date}) {
   const opening = openingHours.map((hour) => {
     // Get hours only
@@ -10,8 +13,17 @@ export function hoursLoad({date}) {
     const isHourPast = dayjs(date).add(scheduleHour, 'hour').isAfter(dayjs())
     return {
       hour,
-      available: isHourPast
+      available: isHourPast,
     }
   })
-  console.log(opening);
+
+  // Render the hours
+  opening.forEach(({hour, available}) => {
+    const li = document.createElement('li')
+    li.classList.add('hour')
+    li.classList.add(available ? 'hour-available' : 'hour-unavailable')
+
+    li.textContent = hour
+    hours.append(li)
+  })
 }
